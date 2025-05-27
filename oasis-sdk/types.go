2a0d848afb1d1d73f74a65a1b32a27a4ee71f9f1
jjj333_p <jjj333.p.1325@gmail.com>
2025-05-27 02:22:49 -1000

    Add support for chat states, receipts, and stanza IDs
    This commit introduces support for XMPP chat states (active, inactive, composing, etc.), delivery and read receipts, and stanza IDs as per XEP specifications. New structures and methods improve message handling and state tracking in `ChatMessageBody`. These changes enhance protocol compliance and message processing capabilities.

--- oasis-sdk/types.go
+++ oasis-sdk/types.go
@@ -37,21 +37,103 @@ type Reply struct {
 	ID      string   `xml:"id,attr"`
 	To      string   `xml:"to,attr"`
 }
+
+// OriginID provided by XEP-0359: Unique and Stable Stanza IDs
 type OriginID struct {
 	XMLName xml.Name `xml:"urn:xmpp:sid:0 origin-id"`
 	ID      string   `xml:"id,attr"`
 }
 
+// DeliveryReceiptRequest provided by XEP-0333: Displayed Markers
+type DeliveryReceiptRequest struct {
+	XMLName xml.Name `xml:"urn:xmpp:receipts request"`
+}
+
+// ReadReceiptRequest provided by XEP-0184: Message Delivery Receipts
+type ReadReceiptRequest struct {
+	XMLName xml.Name `xml:"urn:xmpp:chat-markers:0 markable"`
+}
+
+// ----- begin Chatstates --------
+type GoneChatstate struct {
+	XMLName xml.Name `xml:"http://jabber.org/protocol/chatstates gone"`
+}
+type ActiveChatstate struct {
+	XMLName xml.Name `xml:"http://jabber.org/protocol/chatstates active"`
+}
+type InactiveChatstate struct {
+	XMLName xml.Name `xml:"http://jabber.org/protocol/chatstates inactive"`
+}
+type ComposingChatstate struct {
+	XMLName xml.Name `xml:"http://jabber.org/protocol/chatstates composing"`
+}
+type PausedChatstate struct {
+	XMLName xml.Name `xml:"http://jabber.org/protocol/chatstates paused"`
+}
+
+// ----- end Chatstates --------
+
+type UnknownElement struct {
+	XMLName xml.Name
+	Content string     `xml:",innerxml"`
+	Attrs   []xml.Attr `xml:",any,attr"`
+}
+
 type ChatMessageBody struct {
-	Body              string     `xml:"body"`
-	OriginID          OriginID   `xml:"origin-id"`
-	Reply             Reply      `xml:"reply"`
-	Fallback          []Fallback `xml:"fallback"`
-	Request           *struct{}  `xml:"request"`
-	Markable          *struct{}  `xml:"markable"`
-	FallbacksParsed   bool
-	CleanedBody       string
-	ReplyFallbackText string
+	Body               *string                 `xml:"body"`
+	OriginID           *OriginID               `xml:"origin-id"`
+	StanzaID           *stanza.ID              `xml:"stanza-id"`
+	Reply              *Reply                  `xml:"reply"`
+	Fallback           []Fallback              `xml:"fallback"`
+	Request            *DeliveryReceiptRequest `xml:"request"`
+	Markable           *ReadReceiptRequest     `xml:"markable"`
+	Unknown            []UnknownElement        `xml:",any"`
+	GoneChatState      *GoneChatstate          `xml:"gone"`
+	ActiveChatState    *ActiveChatstate        `xml:"active"`
+	InactiveChatState  *InactiveChatstate      `xml:"inactive"`
+	ComposingChatState *ComposingChatstate     `xml:"composing"`
+	PausedChatState    *PausedChatstate        `xml:"paused"`
+	FallbacksParsed    bool
+	CleanedBody        string
+	ReplyFallbackText  string
+}
+
+func (self *ChatMessageBody) RequestingDeliveryReceipt() bool {
+	return self.Request != nil
+}
+
+func (self *ChatMessageBody) RequestingReadReceipt() bool {
+	return self.Markable != nil
+}
+
+type ChatState int
+
+const (
+	ChatStateNone ChatState = iota
+	ChatStateActive
+	ChatStateInactive
+	ChatStateComposing
+	ChatStatePaused
+	ChatStateGone
+)
+
+func (self *ChatMessageBody) GetChatstate() ChatState {
+	if self.ComposingChatState != nil {
+		return ChatStateComposing
+	}
+	if self.PausedChatState != nil {
+		return ChatStatePaused
+	}
+	if self.ActiveChatState != nil {
+		return ChatStateActive
+	}
+	if self.InactiveChatState != nil {
+		return ChatStateInactive
+	}
+	if self.GoneChatState != nil {
+		return ChatStateGone
+	}
+	return ChatStateNone
 }
 
 /*
@@ -63,7 +145,7 @@ type XMPPChatMessage struct {
 	ChatBody ChatMessageBody
 }
 
-type ChatMessageHandler func(message XMPPChatMessage)
+type ChatMessageHandler func(client *XmppClient, message XMPPChatMessage)
 
 // XmppClient is the end xmpp client object from which everything else works around
 type XmppClient struct {
